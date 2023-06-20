c5549859bda20730c982f9b49082161f4e5dc6bb
legostaev <alexey.legostaev@gmail.com>
2023-06-20 14:06:50 +0300

    небольшие корректировки

--- Core/Infrastructure/Notebook.py
+++ Core/Infrastructure/Notebook.py
@@ -2,13 +2,15 @@ from Core.Models.Note import Note
 
 
 class Notebook:
-	notes_list: list[Note] = []
+	def __init__(self):
+		self.notes_list: list[Note] = []
+		self.db_file_name: str = 'notes_db.json'
 
 	def add_note(self, note: Note) -> None:
 		if len(self.notes_list) == 0:
 			self.notes_list.append(note)
 		else:
-			note.set_id(len(self.notes_list) + 1)
+			note.set_id(self.notes_list[-1].get_id() + 1)
 			self.notes_list.append(note)
 
 	def get_note_by_idx(self, idx: int) -> Note | None:
